5f5c1d5c0a00cb13e9131b9a8943ece4ecc16292
Gleb Zaystev <gleb1336@gmail.com>
2022-09-21 19:38:16 +0300

    Лабораторная 1 выполнена

--- CompanyEmployees/WeatherForecast.cs
+++ CompanyEmployees/WeatherForecast.cs
@@ -1,6 +1,33 @@
-namespace CompanyEmployees
+using NLog;
+using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees;
+
+[Route("[controller]")]
+[ApiController]
+public class WeatherForecastController : ControllerBase
 {
-    public class WeatherForecast
+    private ILoggerManager _logger;
+    public WeatherForecastController(ILoggerManager logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        _logger.LogInfo("¬от информационное сообщение от нашего контроллера значений.");
+       
+        _logger.LogDebug("¬от отладочное сообщение от нашего контроллера значений.");
+       
+        _logger.LogWarn("¬от сообщение предупреждени€ от нашего контроллера значений.");
+       
+        _logger.LogError("¬от сообщение об ошибке от нашего контроллера значений.");
+        return new string[] { "value1", "value2" };
+    }
+}
+
+public class WeatherForecast
     {
         public DateTime Date { get; set; }
 
@@ -10,4 +37,3 @@ namespace CompanyEmployees
 
         public string? Summary { get; set; }
     }
-}
