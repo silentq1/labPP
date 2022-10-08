7cf68d25a89b4aeda543c86204a048732e4d82cf
Gleb <gleb13386@outlook.com>
2022-10-08 23:25:42 +0300

    Revert "4.2 и 4.3"
    This reverts commit b539f002a9c7c4e8639bb0941e3917ca1ecf3bbf.

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -4,7 +4,6 @@ using Contracts;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using Repository;
-using CompanyEmployees;
 
 public static class ServiceExtensions
     {
@@ -16,16 +15,18 @@ public static class ServiceExtensions
             .AllowAnyMethod()
             .AllowAnyHeader());
         });
-        public static void ConfigureIISIntegration(this IServiceCollection services) =>  services.Configure<IISOptions>(options =>
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
         {
 
         });
-        public static void ConfigureLoggerService(this IServiceCollection services) => services.AddScoped<ILoggerManager, LoggerManager>();
-         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
-         services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b =>
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddScoped<ILoggerManager, LoggerManager>();
+         public static void ConfigureSqlContext(this IServiceCollection services,
+         IConfiguration configuration) =>
+         services.AddDbContext<RepositoryContext>(opts =>
+         opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b =>
          b.MigrationsAssembly("CompanyEmployees")));
-        public static void ConfigureRepositoryManager(this IServiceCollection services)=> services.AddScoped<IRepositoryManager, RepositoryManager>();
-        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
-        builder.AddMvcOptions(config => config.OutputFormatters.Add(new
-        CsvOutputFormatter()));
+        public static void ConfigureRepositoryManager(this IServiceCollection services)=>
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
 }
