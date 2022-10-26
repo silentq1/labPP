55005f0d9cd30ffc228ac285fc3a47062840da63
Gleb <gleb13386@outlook.com>
2022-10-26 10:20:13 +0300

    7.2

--- Contracts/IRepositoryManager.cs
+++ Contracts/IRepositoryManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace Contracts
         IShopRepository Shops { get; }
         IOwnerRepository Owner { get; }
         void Save();
+        Task SaveAsync();
 
     }
 }
